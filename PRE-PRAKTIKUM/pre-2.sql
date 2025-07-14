a28bc050a2f3abc246c5cb82d38ad1511362a0fc
tegar-anggoro-suria-valleliant <hytayo55@gmail.com>
2025-07-14 12:48:34 -0700

    Add files via upload

---
+++ PRE-PRAKTIKUM/pre-2.sql
@@ -0,0 +1,14 @@
+-- Menampilkan daftar database
+SHOW DATABASES;
+
+-- Menggunakan database 'mysql'
+USE mysql;
+
+-- Menampilkan daftar tabel dalam database 'mysql'
+SHOW TABLES;
+
+-- Menampilkan deskripsi (struktur) dari tabel 'user'
+DESCRIBE user;
+
+-- Mengambil isi dari kolom 'Host', 'User', dan 'Password' dari tabel 'user'
+SELECT Host, User, Password FROM user;
